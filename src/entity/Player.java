772ae45854fc5270b9acd710e7b2a4558e0b676e
KayTwenty <davidanton037@gmail.com>
2023-07-30 20:40:45 -0400

    Cleaned up player class

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,6 +3,8 @@ package entity;
 import main.gamePanel;
 import main.keyHandler;
 
+import java.awt.*;
+
 public class Player extends Entity {
     gamePanel gp;
     keyHandler keyH;
@@ -10,6 +12,8 @@ public class Player extends Entity {
     public Player(gamePanel gp, keyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        setDefaultValues();
     }
 
     // Set the default values for the Player entity
@@ -17,5 +21,23 @@ public class Player extends Entity {
         x = 100;
         y = 100;
         speed = 4;
+    }
+
+    public void update() {
+        if (keyH.upPressed) {
+            y -= speed;
+        } else if (keyH.downPressed) {
+            y += speed;
+        } else if (keyH.leftPressed) {
+            x -= speed;
+        } else if (keyH.rightPressed) {
+            x += speed;
+        }
+    }
+
+    // Draw the player
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.white);
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
     }
 }
--- src/main/gamePanel.java
+++ src/main/gamePanel.java
@@ -11,7 +11,7 @@ public class gamePanel extends JPanel implements Runnable {
     final int originalTitleSize = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTitleSize * scale; // 48x48 tile
+    public final int tileSize = originalTitleSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -45,7 +45,7 @@ public class gamePanel extends JPanel implements Runnable {
     // Delta Constructor game Loop
     public void run() {
         // Aim at 60 FPS
-        double drawInterval = 1000000000/FPS; // 0.1666 seconds
+        double drawInterval = (double) 1000000000 /FPS; // 0.1666 seconds
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -79,15 +79,7 @@ public class gamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -95,9 +87,7 @@ public class gamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g;
 
-        // Draw the player using Graphics2D
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
 
     }
