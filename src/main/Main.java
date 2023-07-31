90d27af6911424933c4266ebdbe59849f68692d3
KayTwenty <davidanton037@gmail.com>
2023-07-31 19:36:08 -0400

    Added a default logo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,24 @@
 package main;
 
 import javax.swing.*;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+
+        // Window (JFrame) Properties
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Oswald's Quest");
 
+        // Icon Properties
+        URL url = Main.class.getResource("/icons/logo.png");
+        assert url != null;
+        ImageIcon imgIcon = new ImageIcon(url);
+        window.setIconImage(imgIcon.getImage());
+
+        // Add the game panel
         gamePanel gamePanel = new gamePanel();
         window.add(gamePanel);
 
@@ -17,6 +27,7 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        // Start the game
         gamePanel.startGameThread();
     }
 }
